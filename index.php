905f2ef33ee74be674cb1ccfaf0a392ea79117a3
Isaac Butcher <butcheri@papanui.school.nz>
2024-07-13 22:34:36 +1200

    update to PHP

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <html lang="en">
 <head>
     
-// The title for the website
 
+// The title for the website
 <title>Kiwi Cuisine Website test</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
