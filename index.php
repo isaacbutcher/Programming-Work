833722c45fa8070bab681d853c3571c045638f6d
Isaac Butcher <butcheri@papanui.school.nz>
2024-07-13 13:18:52 +1200

    updated code

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+// The language that the code will be in
+<html lang="en">
+<head>
+    
+// The title for the website
+
+<title>Kiwi Cuisine Website test</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
+</head>
+<body>
+    
+</body>
+</html>
